e48e2df25b170077cab3ecb1be2c4d21433fde9f
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-23 12:03:24 +1100

    Add indefinite arguments to function parameters
    Used a for (let removeArg of removeArgs) allowing for more arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,31 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...removeArgs) {
+    
+    
+    
+    for (let removeArg of removeArgs) {
+   
 
+  let resultArray = arr
+    
+           
+     
+      let indexOfItem = resultArray.indexOf(removeArg);
+
+     
+
+      let arrEnd = resultArray.slice(indexOfItem + 1);
+      let arrStart = resultArray.slice(0, indexOfItem);
+
+      
+
+      let newArray = arrStart.concat(arrEnd);
+
+      
+     resultArray = newArray
+      
+      return resultArray
+    }
+
+}
 // Do not edit below this line
 module.exports = removeFromArray;
