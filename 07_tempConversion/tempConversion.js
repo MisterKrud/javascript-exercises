b34f445d4d9a79e0302a15b88776d53a31634f39
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-24 20:29:45 +1100

    Complete exercise
    I did it! Real quick this time, too

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,27 @@
-const convertToCelsius = function() {
+const log = console.log;
+
+const convertToCelsius = function (fahrenheit) {
+  const celsiusConversion = (fahrenheit - 32) * (5 / 9);
+  if (celsiusConversion != Math.trunc(celsiusConversion)) {
+    const celsiusTemp = parseFloat(celsiusConversion.toFixed(1));
+    return celsiusTemp;
+  } else {
+    return celsiusConversion;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const fahrenheitConversion = celsius * (9 / 5) + 32;
+  if (fahrenheitConversion != Math.trunc(fahrenheitConversion)) {
+    const farhenheitTemp = parseFloat(fahrenheitConversion.toFixed(1));
+    return farhenheitTemp;
+  } else {
+    return fahrenheitConversion;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
