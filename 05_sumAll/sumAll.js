b7ca8edeaf93b5bab44d8a3434ef4aa36898dd1e
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-24 19:37:02 +1100

    Solve problem
    This is a bad commmit msg. I solved it
    However, my solution wasn't nearly as elegant as the answer :(

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,46 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  const log = console.log;
 
+  const nums = [num1, num2];
+
+  log(`Argument aray: ${nums}`);
+  let numNegative = nums.find(findNegativeNumber);
+  
+
+  function findNegativeNumber(value, index, array) {
+    return value<0
+  
+  }
+  log(`Negative number? ${numNegative}`)
+
+  let nonInteger = nums.find(findInteger)
+
+  function findInteger(value, index, array) {
+    return !Number.isInteger(value)
+  }
+log(`Integer? ${nonInteger}`)
+
+
+  if (numNegative || nonInteger ) {
+    return "ERROR";
+  } else {
+    let n = 0;
+    if (num1 < num2) {
+      for (let i = num1; i <= num2; i++) {
+        n = i + n;
+      }
+      let solution = n;
+
+      return solution;
+    } else if (num2 < num1) {
+      for (let i = num2; i <= num1; i++) {
+        n = i + n;
+      }
+      let solution = n;
+
+      return solution;
+    }
+  }
 };
 
 // Do not edit below this line
