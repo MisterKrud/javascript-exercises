fb81fb10f7a23446895eb61c81d2733f9c38b1a0
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-25 16:07:10 +1100

    Initial commit
    Added functions for: add, subtract, multiply, sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,33 @@
-const add = function() {
+const log = console.log
+
+const add = function(num1, num2) {
+	const addSolution = (num1 + num2)
+  return addSolution
+};
+
+const subtract = function(num1, num2) {
+	if (num1>num2){
+  const subtractSolution = (num1 - num2)
+  return subtractSolution
+} else  {
+  const subtractSolution = (num2-num1)
+  return subtractSolution
+}
+};
+
+const sum = function(...nums) {
+
+const numberArray = nums
+const sumSolution = (sum(numberArray))
+return sumSolution
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
+const multiply = function(...nums) {
+let num = 1
+for (let i = 0; i<numberArray.length; i++) {
+  num = num*nums[i]
+}
 };
 
 const power = function() {
@@ -21,7 +37,6 @@ const power = function() {
 const factorial = function() {
 	
 };
-
 // Do not edit below this line
 module.exports = {
   add,
