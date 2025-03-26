f1ed174b5e9a64eec46c3c26d1b48b6e959a1ee3
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-26 16:33:53 +1100

    Fix issue where numbers are ignored
    I added '0-9' to the Regex component of replaceAll()
    This fixed the issue

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let lowerCase = word.toLowerCase()
+    console.log(`word: ${word}`)
+    let lettersOnly = lowerCase.replaceAll(/[^a-z0-9]/g, '')
+    console.log(`lettersOnly: ${lettersOnly}`)
+    let forwardWord = lettersOnly.split('').join('');
 
-};
+    let backwardWord = lettersOnly.split('').reverse().join('');
+
+    console.log(`forwardWord: ${forwardWord} | backwardWord: ${backwardWord}`)
+   
+   if (forwardWord != backwardWord) {
+       return false
+   } else {
+       return true
+   }
+   
+   
+   
+   };
+   
 
 // Do not edit below this line
 module.exports = palindromes;
