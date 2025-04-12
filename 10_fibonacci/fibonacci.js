6e0700cc1b4cabbcd8bbd24b80ea021d250a77cf
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-04-12 14:04:00 +1000

    Creat function to give number by Fibonacci position

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(int) {
+    let num = parseInt(int);
+ //if(num===0) {
+   // return 0
+ //} else 
+ if (num<0) {
+    return 'OOPS'
+ } else {
 
-};
+    let arr =[0, 1]
+    for (let n=2; n<=num; n++) {
+          let  newNumber = arr[n-1]+arr[n-2];
+            arr.push(newNumber);
+        }
+return arr[num]
+    }}
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
