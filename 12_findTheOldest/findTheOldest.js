aec16df734f14ff710a1c49f651d639abfefa533
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-04-12 15:17:15 +1000

    Find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
 
-};
+  const findTheOldest = function(people) {
+    const oldestPerson = people.sort((a, b) => {
+    const currentYear = (new Date()).getFullYear(); 
+    
+    if (!a.yearOfDeath){
+a.yearOfDeath = currentYear
+    } 
+    if (!b.yearOfDeath){
+        b.yearOfDeath = currentYear
+    }
+           
+    const person1 = a.yearOfDeath - a.yearOfBirth; 
+
+
+       
+   
+    const person2 = b.yearOfDeath - b.yearOfBirth;
+
+    return person1 > person2 ? -1 : 1;
+    
+
+    });
+    return oldestPerson[0]
+    };
+
+    
 
 // Do not edit below this line
 module.exports = findTheOldest;
